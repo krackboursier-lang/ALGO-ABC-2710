35cb57bc782eb2eb9d5b0d9117fcadd3ccbcc91a
salah <krackboursier@gmail.com>
2025-12-04 14:22:24 +0100

    konda

---
+++ Cours-Konda/TantQue.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TantQue {
+
+    public static void main(String[] args) {
+
+         int note;
+         Scanner sc = new Scanner(System.in); //* Initialsiation du scanner */
+
+         do { 
+
+            System.out.println("Veuillez vous attribuer une note sur 20");
+            note = sc.nextInt();
+             
+         } while(note>=20);
+
+
+                     System.out.println("Vous avez :"+note+"/20");
+        
+    }
+    
+}
