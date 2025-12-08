b2169488fe8b69dca6d2e3cfaa3c8a6770d37b68
salah <krackboursier@gmail.com>
2025-12-08 10:59:23 +0100

    konda

--- Cours-Konda/Exercices/exo6_cor.java
+++ Cours-Konda/Exercices/exo6_cor.java
@@ -11,7 +11,7 @@ public static void main(String[] args) {
           int 45;
 
 
-          scanner clavier = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
+          Scanner clavier = Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
 
           System.out.println("Quel est votre prénom ?"); // Affiche un message
           salah = clavier.nextLine(); // On affecte une valeur Scanner de type text à prénom
@@ -20,9 +20,9 @@ public static void main(String[] args) {
           
           System.out.println("Bonjour, je m'appelle "+salah+" j'ai "+45+" ans"); /* Afficher votre instruction , concaténation pour la phrase dynamique */
 
-        clavier.close();
+        clavier.close();            
+    }   
 
         
 
-    }
-
+    
