4b6dc59b6f948c2cf89558a2a5d9ff47fa8e8738
salah <krackboursier@gmail.com>
2025-12-08 14:34:19 +0100

    kon

--- Cours-Konda/Exercices/exo9_cor.java
+++ Cours-Konda/Exercices/exo9_cor.java
@@ -1,26 +1,68 @@
-package exercices;
+package exercices; /* Déclaration du package */
+import java.util.Scanner; /* Importation de la bibliothèque Java Scanner */
 
-public class exo9_cor {
-    public static void main(String[] args) {
-        
-        int number; // Déclaration d'une variable de type entier
 
-        System .out .println("Please enter a number :"); // Affichage du message pour demander un nombre
+public class exo9_cor { /* Nom de la classe */
 
-        java.util.Scanner clavier = new java.util.Scanner(System.in); // Déclaration du scanner
 
-        number = clavier.nextInt(); // Lecture de l'entier tapé au clavier
+    public static void main(String[] args) { /* Toujours le mettre pour tester votre code */
 
-        if (number % 2 == 0) { // Condition pour vérifier si le nombre est pair
+        /***--------  Début du code  -------------------****/
 
-            System.out.println("The number " + number + " is even."); // Affichage si le nombre est pair
+        String firstname,lastname,email,password,confirmpassword,login; //  Déclaration d'une variable a de Type Texte */
+        Scanner clavier = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
 
-        } else {
+        System.out.println("Votre prénom ?"); /* Afficher votre instruction */
+    
+        firstname = clavier.nextLine(); // Affection de la valeur Scanner tapé sur le clavier à la variable a
 
-            System.out.println("The number " + number + " is odd."); // Affichage si le nombre est impair
+        System.out.println("Votre nom ?"); /* Afficher votre instruction */
 
+        lastname = clavier.nextLine(); // Affection de la valeur Scanner tapé sur le clavier à la variable a
+
+        System.out.println("Votre email ?"); /* Afficher votre instruction */
+
+        email = clavier.nextLine(); // Affection de la valeur Scanner tapé sur le clavier à la variable a
+
+        System.out.println("Votre mot de passe ?"); /* Afficher votre instruction */
+
+        password = clavier.nextLine(); // Affection de la valeur Scanner tapé sur le clavier à la variable a
+
+        System.out.println("Confirmez votre mot de passe ?"); /* Afficher votre instruction */
+
+        confirmpassword = clavier.nextLine(); // Affection de la valeur Scanner tapé sur le clavier à la variable a
+
+        if(!password.equals(confirmpassword)){ // Vérifier si les mots de passes ne sont pas identiques
+
+            System.out.println("inscription annulée "); /* Afficher votre instruction */
+
+
+        }else{
+
+            System.out.println("Merci "+firstname+" "+lastname+", votre inscription a bien été effectuée, vous recevrez un mail de confirmation à l'adresse : "+email); /* Afficher votre instruction */
+            System.out.println("Pour vous connecter, renseignez votre email ?"); /* Afficher votre instruction */
+            login = clavier.nextLine();
+            System.out.println("Renseignez votre mot de passe ?"); /* Afficher votre instruction */
+            confirmpassword = clavier.nextLine();
+
+            // des if dans un if c'est bien mais c'est pas PRO ! la prochaine fois on verra avec les méthodes
+
+            if(confirmpassword.equals(password) && login.equals(email)){ /* Vérifier si le mot de passe et le login sont équivalent au compte créé */
+
+                System.out.println("Connexion acceptée"); /* Afficher votre instruction */
+
+            }else{
+
+                System.out.println("Connexion refusée"); /* Afficher votre instruction */
+
+            }
         }
 
-        clavier.close(); // Fermeture du scanner
-    }
+
+        clavier.close();
+
+      
+
+  }
+
 }
