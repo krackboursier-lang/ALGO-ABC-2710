791617a1ccf3c0be90d3a111aa7575e76a11150e
salah <krackboursier@gmail.com>
2025-12-08 10:00:46 +0100

    konda

---
+++ Cours-Konda/Exercices/exo4_cor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;   
+
+
+
+     int a;
+
+          Scanner clavier = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
+
+          System.out.println("La valeur de a"); // Affiche un message
+          a = clavier.nextInt(); // 
+
+        if (a%2==0){
+
+            System.out.println(a+" est divisible par 2");
+        }else{
+
+            System.out.println(a+" n'est pas divisible par 2");
+        }
+
+
+        clavier.close();
+
+        /***--------  Fin du code  -------------------****/
+
+    }
